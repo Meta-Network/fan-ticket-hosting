4c43009ab3b61b8bf67315fc46f87bc883991da0
Frank Wei <frank@frankwei.xyz>
2021-07-27 15:53:52 +0800

    feat: `operatorWallet`

--- src/wallet/wallet.service.ts
+++ src/wallet/wallet.service.ts
@@ -3,17 +3,30 @@ import {
   Injectable,
   NotFoundException,
 } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Wallet } from 'ethers';
+import { Logger } from '@nestjs/common';
+import { currentProvider } from 'src/constant/providers';
 import { Account } from 'src/entities/Account';
 import { Repository } from 'typeorm';
+import { Token } from '../entities/Token';
+import { TransactionStatus } from 'src/types';
 
 @Injectable()
 export class WalletService {
+  #operatorWallet: Wallet;
+  logger: Logger;
   constructor(
     @InjectRepository(Account)
     private readonly walletAccountRepo: Repository<Account>,
-  ) {}
+    private readonly configService: ConfigService
+  ) {
+    this.logger = new Logger('WalletService')
+    const privateKey = configService.get<string>('operatorWallet.privateKey');
+    this.#operatorWallet = new Wallet(privateKey, currentProvider)
+    this.logger.verbose(`Operator Wallet is ${this.#operatorWallet.address}`)
+  }
 
   static unlockWallet(
     encryptedWallet: string,
@@ -50,5 +63,21 @@ export class WalletService {
     const nextNonce = currentNonce + 1;
     await this.walletAccountRepo.update(account.id, { nonce: nextNonce });
     return { currentNonce, nextNonce };
+  }
+
+  async handleTokenIssueRequest(tokensNeedToDeploy: Token[]): Promise<void> {
+    // anti fool design
+    // PENDING = not sent yet
+    const notPendingTokens = tokensNeedToDeploy.filter(t => t.status !== TransactionStatus.PENDING);
+    if (notPendingTokens.length > 0) {
+      this.logger.verbose(`Not Pending Token Ids: ${notPendingTokens.map(t => t.id).join(', ')}`)
+      throw new Error("handleTokenIssueRequest only receives PENDING tokens. ");
+    }
+
+    // @todo: use Multicall to handle them all
+
+
+    // @todo: update their status to `TransactionStatus.SENDING` with TxHash
+    
   }
 }
