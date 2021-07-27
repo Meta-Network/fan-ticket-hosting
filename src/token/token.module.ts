d5f3ce9633805ea8d25c3c0df1bd8c2234675591
Frank Wei <frank@frankwei.xyz>
2021-07-27 16:38:53 +0800

    feat: `handleTokenIssueRequest`

--- src/token/token.module.ts
+++ src/token/token.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TokenService } from './token.service';
 import { TokenController } from './token.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Account } from 'src/entities/Account';
+import { Transaction } from 'src/entities/Transaction';
+import { Token } from 'src/entities/Token';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Token, Account, Transaction])],
   providers: [TokenService],
-  controllers: [TokenController]
+  controllers: [TokenController],
 })
 export class TokenModule {}
--- src/token/token.service.ts
+++ src/token/token.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { ConflictException, Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { BigNumber } from 'ethers';
 import { Wallet } from 'ethers';
@@ -13,6 +13,9 @@ import {
 } from '../types/contracts';
 import { PermitService } from './permits';
 import { Multicall__factory } from 'src/types/contracts/MulticallFactory';
+import { In, Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Account } from 'src/entities/Account';
 
 @Injectable()
 export class TokenService {
@@ -22,6 +25,8 @@ export class TokenService {
   factoryContract: FanTicketFactory;
 
   constructor(
+    @InjectRepository(Token)
+    private readonly tokenRepo: Repository<Token>,
     private readonly configService: ConfigService
   ) {
     this.factoryContract = FanTicketFactory__factory.connect(
@@ -34,9 +39,8 @@ export class TokenService {
     this.logger.verbose(`Operator Wallet is ${this.#operatorWallet.address}`)
   }
 
-  async handleTokenIssueRequest(tokensNeedToDeploy: Token[]): Promise<void> {
-    // anti fool design
-    // PENDING = not sent yet
+  async handleTokenIssueRequest(): Promise<void> {
+    const tokensNeedToDeploy = await this.tokenRepo.find({ status: TransactionStatus.PENDING })
     const notPendingTokens = tokensNeedToDeploy.filter(
       t => t.status !== TransactionStatus.PENDING,
     );
@@ -67,46 +71,54 @@ export class TokenService {
     // try with call static to see is there anything wrong.
     const estimatedGas = await multicall.estimateGas.aggregate(calls);
     this.logger.verbose(`Estimated Gas for deploying ${calls.length} token(s): ${estimatedGas.toString()}`)
-
+    // 1 blockchain call, that includes all these creations call
     const tx = await multicall.aggregate(calls);
     const hash = tx.hash
     const deployed = tokensNeedToDeploy.map(t => {
       return { ...t, txHash: hash, status: TransactionStatus.SENDING }
     })
     console.info('deployed', deployed)
-    // @todo: update their status to `TransactionStatus.SENDING` with TxHash
-    // this.xxxrepo.update/save(deployed);
+    
+    // update their status to `TransactionStatus.SENDING` with TxHash
+    await this.tokenRepo.update({ id: In(deployed.map(t => t.id)) }, {
+      txHash: hash, status: TransactionStatus.SENDING
+    });
+  }
+
+  private async checkIsLegitToCreate(symbol: string, owner: Account): Promise<void> {
+    const matched = await this.tokenRepo.findOne({ symbol });
+    if (matched) {
+      throw new ConflictException("Sorry, but this symbol was taken by others.")
+    }
+    // @todo: check is `owner` have token
   }
 
   async create(
     name: string,
     symbol: string,
-    tokenId: number,
-    ownerWalletAddress: string,
+    owner: Account,
     initialSupply: BigNumber,
   ): Promise<void> {
-    // @todo: real admin wallet
-    const adminWallet = Wallet.createRandom();
+    // check on `symbol` exist or not
+    await this.checkIsLegitToCreate(symbol, owner)
+
+    const computedAddress = await this.factoryContract.computeAddress(name, symbol);
+    let token = await this.tokenRepo.save({
+      name, symbol, totalSupply: initialSupply.toString(), owner,
+      address: computedAddress,
+    })
     const permit = await PermitService.CreationPermitConstuctor(
       this.factoryContract,
-      adminWallet,
+      this.#operatorWallet,
       name,
       symbol,
-      ownerWalletAddress,
-      tokenId,
+      owner.address,
+      token.id,
       initialSupply,
     );
+    token = { ...token, r: permit.r, s: permit.s, v: permit.v }
     // @todo: write permit to DB and use multicall to `newAPeggedToken`
-    // const tx = await this.factoryContract.newAPeggedToken(
-    //   name,
-    //   symbol,
-    //   ownerWalletAddress,
-    //   initialSupply,
-    //   tokenId,
-    //   permit.v,
-    //   permit.r,
-    //   permit.s,
-    // );
+    await this.tokenRepo.save(token)
     // return tx.hash;
   }
 
