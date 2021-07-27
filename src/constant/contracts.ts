a80b08669f69c0f8e02fcb820ea7699ee814eb36
Frank Wei <frank@frankwei.xyz>
2021-07-27 15:57:25 +0800

    feat: Multicall

--- src/constant/contracts.ts
+++ src/constant/contracts.ts
@@ -1,5 +1,9 @@
 import { ChainId, currentChainId } from '.';
 
+export type AddressBookForNetwork = {
+  [chainId in ChainId]: string;
+};
+
 export const CONTRACTS: {
   [chainId in ChainId]?: {
     [contractName: string]: string;
@@ -26,4 +30,15 @@ export const CONTRACTS: {
   },
 };
 
+export const MULTICALL_NETWORKS: AddressBookForNetwork = {
+  [ChainId.MAINNET]: '0xeefBa1e63905eF1D7ACbA5a8513c70307C1cE441',
+  [ChainId.ROPSTEN]: '0x53C43764255c17BD724F74c4eF150724AC50a3ed',
+  [ChainId.KOVAN]: '0x2cc8688C5f75E365aaEEb4ea8D6a480405A48D2A',
+  [ChainId.RINKEBY]: '0x42Ad527de7d4e9d9d011aC45B31D8551f8Fe9821',
+  [ChainId.GÖRLI]: '0x77dCa2C955b15e9dE4dbBCf1246B4B85b651e50e',
+  [ChainId.BSC_MAINNET]: '0xe348b292e8eA5FAB54340656f3D374b259D658b8',
+  [ChainId.BSC_TESTNET]: '0xe348b292e8eA5FAB54340656f3D374b259D658b8',
+};
+
 export const currentContracts = CONTRACTS[currentChainId];
+export const currentMulticall = MULTICALL_NETWORKS[currentChainId];
