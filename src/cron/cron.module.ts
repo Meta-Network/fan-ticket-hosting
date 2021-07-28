8159bd5dc99f0e5bdda479457336ac16a0579346
Frank Wei <frank@frankwei.xyz>
2021-07-28 14:59:04 +0800

    feat: start clearing module

--- src/cron/cron.module.ts
+++ src/cron/cron.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TokenModule as TokenCronModule } from './token/token.module';
 import { GasLimitModule } from './gas-limit/gas-limit.module';
+import { ClearingService } from './clearing/clearing.service';
 
 @Module({
   imports: [TokenCronModule, GasLimitModule],
+  providers: [ClearingService],
 })
 export class CronModule {}
