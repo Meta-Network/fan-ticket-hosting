2485afbb55167b4a719e3e5a4a36bd6c165f08ad
Frank Wei <frank@frankwei.xyz>
2021-08-02 16:52:56 +0800

    fix: use cookie parser

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get<ConfigService>(ConfigService);
 
+  app.use(cookieParser());
   const runningOnPort = configService.get<number>('app.port') || 3000;
   console.info(`Running on Port${runningOnPort}`);
 
