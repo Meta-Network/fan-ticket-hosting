90f28582ac3a212e9c1ac551ddf3f7c30d8d12c4
Frank Wei <frank@frankwei.xyz>
2021-08-10 16:17:54 +0800

    feat: basic service for `InterchainService`

--- src/token/interchain/interchain.service.ts
+++ src/token/interchain/interchain.service.ts
@@ -1,4 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotImplementedException } from '@nestjs/common';
+import { BigNumber } from 'ethers';
+import { InterChainToken } from 'src/entities/InterChainToken';
+import { Token } from 'src/entities/Token';
 
 @Injectable()
-export class InterchainService {}
+export class InterchainService {
+    async requestInterChainCreationPermit(token: Token) {
+
+        throw new NotImplementedException("T.B.I");
+    }
+    async mintInterChainToken(token: InterChainToken, to: string, value: BigNumber) {
+        throw new NotImplementedException("T.B.I");
+    }
+    async redeemInterChainToken(token: InterChainToken, txHash: string) {
+        // @todo: check is tx exist in DB or not
+
+        // @todo: check txHash is burnt or not
+        throw new NotImplementedException("T.B.I");
+    }
+}
