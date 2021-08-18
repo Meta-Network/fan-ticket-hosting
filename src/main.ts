c956eaf5f8e85c59462195a0d913d2e4fd426f3f
Frank Wei <frank@frankwei.xyz>
2021-08-18 18:29:22 +0800

    fix: enable for dev

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get<ConfigService>(ConfigService);
-
+  app.enableCors({
+    origin: [
+      'http://localhost:3000'
+    ]
+  })
   app.use(cookieParser());
   const runningOnPort = configService.get<number>('app.port') || 3000;
   console.info(`Running on Port${runningOnPort}`);
